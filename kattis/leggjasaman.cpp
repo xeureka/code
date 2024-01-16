a265a4e5f11781cc1c947ad605ef65f5d8bb86dd
Eureka <teklemariameureka@gmail.com>
2024-01-16 09:55:32 +0300

    Add kattis

---
+++ kattis/leggjasaman.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+
+int main()
+{
+    int arnar,hannes;
+    
+    cin >>arnar>>hannes;
+    
+    
+    cout << arnar + hannes<<endl;
+    return 0;
+}
---
+++ kattis/ovissa.py
@@ -0,0 +1,11 @@
+word = input().lower()
+
+
+count = 0
+
+
+for i in word:
+    if i == 'u':
+        count +=1
+        
+print(count)
