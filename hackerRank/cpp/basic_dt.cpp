930728e92aad459b72d1547bcc6782698237d566
Eureka <teklemariameureka@gmail.com>
2024-01-16 10:59:08 +0300

    Add hackerRank/cpp/

---
+++ hackerRank/cpp/basic_dt.cpp
@@ -0,0 +1,25 @@
+
+//https://www.hackerrank.com/challenges/c-tutorial-basic-data-types/problem?isFullScreen=true
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int intValue;
+    long longValue;
+    char charValue;
+    float floatValue;
+    double doubleValue;
+
+    cin >> intValue >> longValue >> charValue >> floatValue >> doubleValue;
+
+    cout << intValue << endl;
+    cout << longValue << endl;
+    cout << charValue << endl;
+    cout << fixed << floatValue << endl;
+    cout << fixed << doubleValue << endl;
+
+  
+      
+  }
---
+++ hackerRank/cpp/hello.cpp
@@ -0,0 +1,14 @@
+
+//https://www.hackerrank.com/challenges/cpp-hello-world
+
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    
+    
+    cout <<"Hello, World!"<<endl;
+    return 0;
+}
